7557dd08cf19dcaf4e22ecbda38ec94bba6ffb93
Eamon White <eamon.white7@gmail.com>
2017-06-29 20:05:21 -0400

    front end sign in

---
+++ src/pages/signin/signin.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-sign-in',
+  templateUrl: 'signin.html'
+})
+export class SignInPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
