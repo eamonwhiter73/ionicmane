969344e8003dc0235d619e791d19330aa0d66709
Eamon White <eamon.white7@gmail.com>
2017-10-17 12:46:24 -0400

    finished arrows on stylistprofile

--- www/build/2.js
+++ www/build/2.js
@@ -1,6 +1,6 @@
 webpackJsonp([2],{
 
-/***/ 812:
+/***/ 813:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- www/build/4.js
+++ www/build/4.js
@@ -1,6 +1,6 @@
 webpackJsonp([4],{
 
-/***/ 810:
+/***/ 811:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
